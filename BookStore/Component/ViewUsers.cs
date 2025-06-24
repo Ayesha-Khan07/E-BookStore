9a9a120d315f8a942fcdf67a38ae57e6adaca53a
Ayesha-Khan07 <89073826+Ayesha-Khan07@users.noreply.github.com>
2025-06-24 15:21:48 +0500

    Add files via upload

---
+++ BookStore/Component/ViewUsers.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BookStore.Models;
+
+namespace _2ndBookStore.Component
+{
+    public class ViewUsers : ViewComponent
+    {
+        public List<Users> Invoke()
+        {
+            AdminRepo rep = new AdminRepo();
+            List<Users> ulist = rep.getAllUsers();
+
+            return ulist;
+        }
+        
+    }
+}
